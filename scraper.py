1079f3b854c3e66ff8485a28c57404c7b5163437
Alex Yule <adyule@gmail.com>
2014-06-07 10:19:09 -0700

    make sure to scrape page 0

--- scraper.py
+++ scraper.py
@@ -24,6 +24,9 @@ def scrape_genre(genre):
 
     pages = count / 500
     if not count % 500: pages -= 1
+
+    scrape_genre_page(genre, 0)
+    
     #if not check_cache(genre, 0):
         #print "not cached, processing json"
         #process_json(page, genre)
