ab84afecb648a2323294d83f433b0ed08c85c918
Alex Yule <adyule@gmail.com>
2019-03-26 17:30:02 -0700

    geojson for now

--- export.py
+++ export.py
@@ -8,15 +8,27 @@ def get_geocoded_bands(limit=''):
     if limit: limit = " LIMIT " + str(limit)
     return scraperwiki.sqlite.select("* FROM swdata where geo_geom IS NOT NULL and geo_place_name <> '-1'" + limit)
 
-bands = []
+# bands = []
+PRECISION = 6
 
 print('{"features": [')
-for band in get_geocoded_bands():
-    # band_center = ast.literal_eval(band['geo_center']) # parse the array
+bands_list = get_geocoded_bands()
+
+for i, band in enumerate(bands_list):
+    # band_center = ast.literal_eval(band['geo_center']) # should be the same as geom, but just in case
     band_geom = ast.literal_eval(band['geo_geom'])
+
+    # Rounding to save space if needed:
+    # coords=list(map(lambda c: round(c, PRECISION), band_geom['coordinates']))
+    # band_geom['coordinates'] = coords
+
     band_feature = Feature(geometry=band_geom, properties=band)
-    bands.append(band_feature)
-    print(json.dumps(band_feature)+',') # for tippecanoe --read-parallel
+    # bands.append(band_feature)
+    if i < len(bands_list)-1:
+        print(geojson.dumps(band_feature) + ',')
+    else:
+        print(geojson.dumps(band_feature))
+
 
 print('], "type": "FeatureCollection"}')
 # print(geojson.dumps(FeatureCollection(bands)))
