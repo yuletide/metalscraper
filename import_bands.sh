6a25fbdf443630e70dd062640eaee5254b5cd9ed
Alex Yule <adyule@gmail.com>
2020-11-27 23:35:22 -0500

    vector clusters at 30

--- import_bands.sh
+++ import_bands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Exporting scraper to geojson"
-pipenv run python export.py > geojson/bands.geojson
+# pipenv run python export.py > geojson/bands.geojson
 echo "Importing geojson to postgres"
-ogr2ogr -progress "PG:host=localhost dbname=postgres" geojson/bands.geojson -nln bands.bands -overwrite
+# ogr2ogr -progress "PG:host=localhost dbname=postgres" geojson/bands.geojson -nln bands.bands -overwrite
 # cd geojson && mtsds --sync
-# tippecanoe -z14 -o ../mbtiles/bands_cluster.mbtiles -r1 --cluster-distance=20 bands.geojsonl --read-parallel --force
+tippecanoe -z14 -o ../mbtiles/bands_cluster.mbtiles -r1 --cluster-distance=30 bands.geojsonl --read-parallel --force --layer=bands
